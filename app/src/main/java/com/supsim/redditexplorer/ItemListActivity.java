9d18f791bd2cf6e4f9130fa09b291ce1a71c9ec0
CJLWS <john@cjlws.co.uk>
2017-08-23 23:42:55 +0100

    Test from Air

--- app/src/main/java/com/supsim/redditexplorer/ItemListActivity.java
+++ app/src/main/java/com/supsim/redditexplorer/ItemListActivity.java
@@ -48,7 +48,7 @@ public class ItemListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action - will do", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
